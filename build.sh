6125950fb775478bf2f054c3acba84dfe5176c66
Guillermo David Andrada <guillermoandrada@gmail.com>
2024-11-04 02:46:34 -0300

    fix: borrar pip i requirements

--- build.sh
+++ build.sh
@@ -2,9 +2,6 @@
 # exit on error
 set -o errexit
 
-# install dependencies
-pip install -r requirements.txt
-
 # install the project
 python manage.py collectstatic --no-input
 python manage.py migrate
