c8978b88b9f2f31d40b9546c1523865874cf5015
Guillermo David Andrada <guillermoandrada@gmail.com>
2024-11-04 03:02:51 -0300

    fix: deploy installations

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 # exit on error
 set -o errexit
 
+# install dependencies
+pip install -r requirements.txt
+
 # install the project
 python manage.py collectstatic --no-input
 python manage.py migrate
